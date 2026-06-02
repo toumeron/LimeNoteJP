1ceaa9ea31f91a8613ec1256fb417096345e18b8
“toumeron” <toumeron.sub@gmail.com>
2026-06-03 03:25:26 +0900

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/LimeNoteJP/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: "dist",
   },
 });
